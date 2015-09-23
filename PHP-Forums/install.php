6814dcaf21e950bf8979580a47813955edfc9f36
Timothy Gibbons <tjgibbons@gmail.com>
2015-09-22 23:23:58 -0500

    Install...

--- PHP-Forums/install.php
+++ PHP-Forums/install.php
@@ -1,6 +1,8 @@
 <?php
+define('path', '');
+require path.'inc/init.php';
 class DBHelper{
-  private $_db, $_smessage, $_emessage, $_error = true;
+  private $_db, $_smessage, $_emessage, $_error = false;
   
   public function __construct(){
     $this->_db = DB::getInstance();
@@ -16,9 +18,9 @@ class DBHelper{
     	"CREATE TABLE `settings` (`id` INT(11) NOT NULL AUTO_INCREMENT,`name` TEXT NOT NULL,`value` TEXT NULL, PRIMARY KEY (`id`)) COLLATE='latin1_swedish_ci' ENGINE=InnoDB",
     	"CREATE TABLE `users` (`id` INT(11) NOT NULL AUTO_INCREMENT,`username` VARCHAR(50) NOT NULL,`password` LONGTEXT NOT NULL,`salt` LONGTEXT NOT NULL,`name` VARCHAR(50) NOT NULL,`email` TEXT NOT NULL,`group` INT(11) NOT NULL,`joined` DATETIME NOT NULL,PRIMARY KEY (`id`)) COLLATE='latin1_swedish_ci' ENGINE=InnoDB",
     	"CREATE TABLE `user_session` (`id` INT(11) NOT NULL AUTO_INCREMENT,`user_id` INT(11) NOT NULL,`hash` LONGTEXT NOT NULL,PRIMARY KEY (`id`)) COLLATE='latin1_swedish_ci' ENGINE=InnoDB",
-    	'INSERT INTO `groups` (`group_name`, `permissions`) VALUES (`Guest`, `{"Post":1, "Admin":0,"Mod":0}`)',
-    	'INSERT INTO `groups` (`group_name`, `permissions`) VALUES (`Mod`, `{"Post":1, "Admin":0,"Mod":1}`)',
-      	'INSERT INTO `groups` (`group_name`, `permissions`) VALUES (`Administrator`, `{"Post":1, "Admin":1,"Mod":1}`)',
+    	//'INSERT INTO `groups` (`group_name`, `permissions`) VALUES (`Guest`, `{"Post":1, "Admin":0,"Mod":0}`)',
+    	//'INSERT INTO `groups` (`group_name`, `permissions`) VALUES (`Mod`, `{"Post":1, "Admin":0,"Mod":1}`)',
+      	//'INSERT INTO `groups` (`group_name`, `permissions`) VALUES (`Administrator`, `{"Post":1, "Admin":1,"Mod":1}`)',
     );
     $i = 0;
     foreach($tables as $table){
@@ -27,23 +29,66 @@ class DBHelper{
         $this->addMessage('Query index '.$i.' has been added');
       }else{
         $this->addError('There was an error while adding table at index '.$i);
-     }
+      }
       $i++;
     }
     
-    $insert1 = $this->_db->insert('groups', array(
-    	'group_name' => 'Regular',
-    	'permissions' => '{"Admin":0,"Mod":0,"Create":0,"Post":0}'
-    ));
-    $insert2 = $this->_db->insert('groups', array(
-    		'group_name' => 'Administrator',
-    		'permissions' => '{"Admin":1,"Mod":1,"Create":1,"Post":1}'
-    ));
+    $insert = array(
+    		0 =>array(
+    		0=> array(
+    			'table'=>'groups',
+    			'group_name' => 'Register',
+    			'permissions'=>'{"Post":1, "Admin":0,"Mod":0}',
+    		),
+    		),
+    		1=> array(
+    		1 => array(
+    			'table'=>'groups',
+    			'group_name'=> 'Mod',
+    			'permissions'=> '{"Post":1, "Admin":0,"Mod":1}',
+    		),
+    		),
+    		2=>array(
+    		2 => array(
+    			'table' => 'groups',
+    			'group_name'=> 'Admin',
+    			'permissions'=>'{"Post":1, "Admin":1,"Mod":1}',
+    		),
+    		),
+    		3 => array(
+    		3 => array(
+    			'table'=>'settings',
+    			'name'=>'install',
+    			'value' => '1',
+    		),
+    		),
+    		4 => array(
+    		4 => array(
+    			'table'=>'settings',
+    			'name' =>'title',
+    			'value' => 'PHP-Foro',
+    		),
+    		),
+    		5 => array(
+    		5 => array(
+    			'table'=>'settings',
+    			'name'=> 'bootstrap-theme',
+    			'value'=>'cosmo',	
+    		),
+    		),
+    );
     
-    if(!$insert1 && !$insert2){
-    	$this->addError('There was a problem adding rows');
-    }else{
-    	$this->addMessage('Rows added!');
+    $j = 0;
+    foreach ($insert as $ins){
+    	if(!$this->_db->insert($ins[$j]['table'], array(
+    			array_keys($ins[$j])[1]=> array_values($ins[$j])[1],
+    			array_keys($ins[$j])[2]=> array_values($ins[$j])[2],
+    	))){
+    		$this->addMessage('There was an error adding row: '.$j);
+    	}else{
+    		echo $j.' row added!';
+    	}
+    	$j++;
     }
     
     if(!empty($this->_emessage)){
