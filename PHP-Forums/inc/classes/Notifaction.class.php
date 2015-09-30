990ae4b711f494b7fc1aed7f32a3b6b2cd429fe9
Timothy Gibbons <Root3287@users.noreply.github.com>
2015-09-30 09:28:57 -0500

    Copyright

--- PHP-Forums/inc/classes/Notifaction.class.php
+++ PHP-Forums/inc/classes/Notifaction.class.php
@@ -1,4 +1,9 @@
 <?php
+/*
+* Timothy Gibbons
+* Copyright (c) Timothy Gibbons 2015, All Rights Reserved; 
+* License: MIT
+*/
 class Notifaction{
 	public static function get($user){
 		$db = DB::getInstance();
