fce872fe482c234f37e2796b09e559cf0cae9549
Timothy Gibbons <tjgibbons@gmail.com>
2015-10-02 23:01:25 -0500

    Remove old.

--- PHP-Forums/old/assets/foot.php
+++
@@ -1,3 +0,0 @@
-<!-- Latest compiled and minified JavaScript -->
-<script src="<?php echo path.'assets/js/jquery.js';?>"></script>
-<script src="<?php echo path.'assets/js/bootstrap.js';?>"></script>
--- PHP-Forums/old/inc/classes/Redirect.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Redirect{
-	public static function to($location){
-		if($location){
-			header('Location: '.$location);
-			exit();
-		}
-	}
-}
