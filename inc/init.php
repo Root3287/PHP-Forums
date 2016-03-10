8375fb38138bbf961da6021f6c585e46bdeb771a
Timothy Gibbons <tjgibbons@gmail.com>
2016-03-10 02:07:30 -0600

    original inc and install

--- inc/init.php
+++ inc/init.php
@@ -1,22 +1,4 @@
 <?php
-$GLOBALS['config'] = array(
-		"config"=>array("name" => "Social-Media"),
-		"mysql" => array(
-		"host" => "127.0.0.1", //127.0.0.1.
-		"user" => "root", //root
-		"password" => "root", //password
-		"db" => "forums", //forums
-		"port" => "3306", //3306
-	),
-	"remember" => array(
-		"expiry" => 604800,
-	),
-	"session" => array (
-		"token_name" => "forums",
-		"cookie_name"=>"forums",
-		"session_name"=>"forums"
-	),
-);
 
 session_start();
 
